92b3177da5c38d04d9dd9891a8be23e9dc293b13
John Behan <behanj@gmail.com>
2023-01-09 21:01:19 +0000

    up to chapter 8

---
+++ src/systems/player_input.rs
@@ -0,0 +1,36 @@
+use crate::{prelude::*, turn_state};
+
+#[system]
+#[read_component(Point)]
+#[read_component(Player)]
+pub fn player_input(
+    ecs: &mut SubWorld,
+    commands: &mut CommandBuffer,
+    #[resource] key: &Option<VirtualKeyCode>,
+    #[resource] turn_state: &mut TurnState,
+) {
+    if let Some(key) = key {
+        let delta = match key {
+            VirtualKeyCode::Left => Point::new(-1, 0),
+            VirtualKeyCode::Right => Point::new(1, 0),
+            VirtualKeyCode::Up => Point::new(0, -1),
+            VirtualKeyCode::Down => Point::new(0, 1),
+            _ => Point::zero(),
+        };
+
+        if delta.x != 0 || delta.y != 0 {
+            let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+            players.iter_mut(ecs).for_each(|(entity, pos)| {
+                let destination = *pos + delta;
+                commands.push((
+                    (),
+                    WantsToMove {
+                        entity: *entity,
+                        destination,
+                    },
+                ));
+            });
+            *turn_state = TurnState::PlayerTurn;
+        }
+    }
+}
