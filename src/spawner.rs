a23d006697b202127c58d25a304f85fb1aa0c641
John Behan <behanj@gmail.com>
2023-01-16 22:21:13 +0000

    add a game over screen

--- src/spawner.rs
+++ src/spawner.rs
@@ -8,10 +8,7 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             color: ColorPair::new(WHITE, BLACK),
             glyph: to_cp437('@'),
         },
-        Health {
-            current: 20,
-            max: 20,
-        },
+        Health { current: 5, max: 5 },
     ));
 }
 
